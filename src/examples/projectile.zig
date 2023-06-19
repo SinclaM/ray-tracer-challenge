8da9d39e9289d48c8f422f7707cda4d5c4c9471b
SinclaM <matthewsinclair2024@u.northwestern.edu>
2023-06-19 17:43:15 +0100

    More sensible Tuple definition

--- src/examples/projectile.zig
+++ src/examples/projectile.zig
@@ -3,13 +3,13 @@ const print = @import("std").debug.print;
 const Tuple = @import("../raytracer/tuple.zig").Tuple;
 
 const Projectile = struct {
-    position: Tuple,
-    velocity: Tuple,
+    position: Tuple(f32),
+    velocity: Tuple(f32),
 };
 
 const Environment = struct {
-    gravity: Tuple,
-    wind: Tuple,
+    gravity: Tuple(f32),
+    wind: Tuple(f32),
 
     pub fn tick(self: Environment, proj: *Projectile) void {
         proj.* = Projectile { .position = proj.position.add(proj.velocity),
@@ -18,10 +18,10 @@ const Environment = struct {
 };
 
 pub fn simulate() void {
-    var proj = Projectile { .position = Tuple.new_point(0.0, 1.0, 0.0),
-                            .velocity = Tuple.new_vec3(1.0, 1.0, 1.0)};
-    const env = Environment { .gravity = Tuple.new_vec3(0.0, -0.1, 0.0),
-                              .wind = Tuple.new_vec3(-0.01, 0.0, 0.0)};
+    var proj = Projectile { .position = Tuple(f32).new_point(0.0, 1.0, 0.0),
+                            .velocity = Tuple(f32).new_vec3(1.0, 1.0, 1.0)};
+    const env = Environment { .gravity = Tuple(f32).new_vec3(0.0, -0.1, 0.0),
+                              .wind = Tuple(f32).new_vec3(-0.01, 0.0, 0.0)};
 
     while (proj.position.y > 0) {
         print("{}\n", .{ proj.position });
--- src/raytracer/tuple.zig
+++ src/raytracer/tuple.zig
@@ -1,151 +1,153 @@
 const std = @import("std");
 const testing = std.testing;
 
-const tolerance = @import("../definitions.zig").tolerance;
-const F = @import("../definitions.zig").F;
 
-pub const Tuple = struct {
-    const Self = @This();
+pub fn Tuple(comptime T: type) type {
+    return packed struct {
+        const Self = @This();
+        const tolerance: T = 1e-5;
 
-    x: F,
-    y: F,
-    z: F,
-    w: F,
+        x: T,
+        y: T,
+        z: T,
+        w: T,
 
-    pub inline fn new(x: F, y: F, z: F, w: F) Self {
-        return .{ .x = x, .y = y, .z = z, .w = w };
-    }
+        pub inline fn new(x: T, y: T, z: T, w: T) Self {
+            return .{ .x = x, .y = y, .z = z, .w = w };
+        }
 
-    pub inline fn new_point(x: F, y: F, z: F) Self {
-        return .{ .x = x, .y = y, .z = z, .w = 1.0 };
-    }
+        pub inline fn new_point(x: T, y: T, z: T) Self {
+            return .{ .x = x, .y = y, .z = z, .w = 1.0 };
+        }
 
-    pub inline fn new_vec3(x: F, y: F, z: F) Self {
-        return .{ .x = x, .y = y, .z = z, .w = 0.0 };
-    }
+        pub inline fn new_vec3(x: T, y: T, z: T) Self {
+            return .{ .x = x, .y = y, .z = z, .w = 0.0 };
+        }
 
-    pub inline fn approx_equal(self: Self, other: Self) bool {
-        return @fabs(self.x - other.x) < tolerance
-            and @fabs(self.y - other.y) < tolerance
-            and @fabs(self.z - other.z) < tolerance
-            and @fabs(self.w - other.w) < tolerance;
-    }
+        pub inline fn approx_equal(self: Self, other: Self) bool {
+            return @fabs(self.x - other.x) < tolerance
+                and @fabs(self.y - other.y) < tolerance
+                and @fabs(self.z - other.z) < tolerance
+                and @fabs(self.w - other.w) < tolerance;
+        }
 
-    pub inline fn add(self: Self, other: Self) Self {
-        return .{ .x = self.x + other.x,
-                  .y = self.y + other.y,
-                  .z = self.z + other.z,
-                  .w = self.w + other.w };
-    }
+        pub inline fn add(self: Self, other: Self) Self {
+            return .{ .x = self.x + other.x,
+                      .y = self.y + other.y,
+                      .z = self.z + other.z,
+                      .w = self.w + other.w };
+        }
 
-    pub inline fn sub(self: Self, other: Self) Self {
-        return .{ .x = self.x - other.x,
-                  .y = self.y - other.y,
-                  .z = self.z - other.z,
-                  .w = self.w - other.w };
-    }
+        pub inline fn sub(self: Self, other: Self) Self {
+            return .{ .x = self.x - other.x,
+                      .y = self.y - other.y,
+                      .z = self.z - other.z,
+                      .w = self.w - other.w };
+        }
 
-    pub inline fn negate(self: Self) Self {
-        return .{ .x = - self.x,
-                  .y = - self.y,
-                  .z = - self.z,
-                  .w = - self.w };
-    }
+        pub inline fn negate(self: Self) Self {
+            return .{ .x = - self.x,
+                      .y = - self.y,
+                      .z = - self.z,
+                      .w = - self.w };
+        }
 
-    pub inline fn mul(self: Self, val: F) Self {
-        return .{ .x = self.x * val,
-                  .y = self.y * val,
-                  .z = self.z * val,
-                  .w = self.w * val };
-    }
+        pub inline fn mul(self: Self, val: T) Self {
+            return .{ .x = self.x * val,
+                      .y = self.y * val,
+                      .z = self.z * val,
+                      .w = self.w * val };
+        }
 
-    pub inline fn div(self: Self, val: F) Self {
-        return .{ .x = self.x / val,
-                  .y = self.y / val,
-                  .z = self.z / val,
-                  .w = self.w / val };
-    }
+        pub inline fn div(self: Self, val: T) Self {
+            return .{ .x = self.x / val,
+                      .y = self.y / val,
+                      .z = self.z / val,
+                      .w = self.w / val };
+        }
 
-    pub inline fn magnitude(self: Self) F {
-        return @sqrt(self.x * self.x + self.y * self.y + self.z * self.z + self.w * self.w);
-    }
+        pub inline fn magnitude(self: Self) T {
+            return @sqrt(self.x * self.x + self.y * self.y + self.z * self.z + self.w * self.w);
+        }
 
-    pub inline fn normalized(self: Self) Self {
-        return self.div(self.magnitude());
-    }
+        pub inline fn normalized(self: Self) Self {
+            return self.div(self.magnitude());
+        }
 
-    pub inline fn dot(self: Self, other: Self) F {
-        return self.x * other.x + self.y * other.y + self.z * other.z + self.w * other.w;
-    }
+        pub inline fn dot(self: Self, other: Self) T {
+            return self.x * other.x + self.y * other.y + self.z * other.z + self.w * other.w;
+        }
 
-    pub inline fn cross(self: Self, other: Self) Self {
-        return Self.new_vec3(
-            self.y * other.z - self.z * other.y,
-            self.z * other.x - self.x * other.z,
-            self.x * other.y - self.y * other.x
-        );
-    }
-};
+        pub inline fn cross(self: Self, other: Self) Self {
+            return Self.new_vec3(
+                self.y * other.z - self.z * other.y,
+                self.z * other.x - self.x * other.z,
+                self.x * other.y - self.y * other.x
+            );
+        }
 
-test "tuple ops" {
+    };
+}
+
+test "Tuple ops" {
     // addition
-    var a1 = Tuple.new_point(3.0, -2.0, 5.0);
-    var a2 = Tuple.new_vec3(-2.0, 3.0, 1.0);
-    try testing.expect(a1.add(a2).approx_equal(Tuple.new_point(1, 1, 6)));
+    var a1 = Tuple(f32).new_point(3.0, -2.0, 5.0);
+    var a2 = Tuple(f32).new_vec3(-2.0, 3.0, 1.0);
+    try testing.expect(a1.add(a2).approx_equal(Tuple(f32).new_point(1.0, 1.0, 6.0)));
 
     // subtraction
-    a1 = Tuple.new_point(3.0, 2.0, 1.0);
-    a2 = Tuple.new_point(5.0, 6.0, 7.0);
-    try testing.expect(a1.sub(a2).approx_equal(Tuple.new_vec3(-2.0, -4.0, -6.0)));
+    a1 = Tuple(f32).new_point(3.0, 2.0, 1.0);
+    a2 = Tuple(f32).new_point(5.0, 6.0, 7.0);
+    try testing.expect(a1.sub(a2).approx_equal(Tuple(f32).new_vec3(-2.0, -4.0, -6.0)));
 
-    a1 = Tuple.new_point(3.0, 2.0, 1.0);
-    a2 = Tuple.new_vec3(5.0, 6.0, 7.0);
-    try testing.expect(a1.sub(a2).approx_equal(Tuple.new_point(-2.0, -4.0, -6.0)));
+    a1 = Tuple(f32).new_point(3.0, 2.0, 1.0);
+    a2 = Tuple(f32).new_vec3(5.0, 6.0, 7.0);
+    try testing.expect(a1.sub(a2).approx_equal(Tuple(f32).new_point(-2.0, -4.0, -6.0)));
 
-    a1 = Tuple.new_vec3(3.0, 2.0, 1.0);
-    a2 = Tuple.new_vec3(5.0, 6.0, 7.0);
-    try testing.expect(a1.sub(a2).approx_equal(Tuple.new_vec3(-2.0, -4.0, -6.0)));
+    a1 = Tuple(f32).new_vec3(3.0, 2.0, 1.0);
+    a2 = Tuple(f32).new_vec3(5.0, 6.0, 7.0);
+    try testing.expect(a1.sub(a2).approx_equal(Tuple(f32).new_vec3(-2.0, -4.0, -6.0)));
 
     // negation
-    a1 = Tuple.new(1.0, -2.0, 3.0, -4.0);
-    try testing.expect(a1.negate().approx_equal(Tuple.new(-1.0, 2.0, -3.0, 4.0)));
+    a1 = Tuple(f32).new(1.0, -2.0, 3.0, -4.0);
+    try testing.expect(a1.negate().approx_equal(Tuple(f32).new(-1.0, 2.0, -3.0, 4.0)));
 
     // scalar multiplication
-    a1 = Tuple.new(1.0, -2.0, 3.0, -4.0);
-    try testing.expect(a1.mul(3.5).approx_equal(Tuple.new(3.5, -7.0, 10.5, -14.0)));
+    a1 = Tuple(f32).new(1.0, -2.0, 3.0, -4.0);
+    try testing.expect(a1.mul(3.5).approx_equal(Tuple(f32).new(3.5, -7.0, 10.5, -14.0)));
 
     // scalar division
-    a1 = Tuple.new(1.0, -2.0, 3.0, -4.0);
-    try testing.expect(a1.div(2.0).approx_equal(Tuple.new(0.5, -1.0, 1.5, -2.0)));
+    a1 = Tuple(f32).new(1.0, -2.0, 3.0, -4.0);
+    try testing.expect(a1.div(2.0).approx_equal(Tuple(f32).new(0.5, -1.0, 1.5, -2.0)));
 
     // magnitude
-    a1 = Tuple.new_vec3(1.0, 0.0, 0.0);
-    try testing.expectApproxEqAbs(a1.magnitude(), 1.0, tolerance);
+    a1 = Tuple(f32).new_vec3(1.0, 0.0, 0.0);
+    try testing.expectApproxEqAbs(a1.magnitude(), 1.0, Tuple(f32).tolerance);
 
-    a1 = Tuple.new_vec3(1.0, 2.0, 3.0);
-    try testing.expectApproxEqAbs(a1.magnitude(), @sqrt(14.0), tolerance);
+    a1 = Tuple(f32).new_vec3(1.0, 2.0, 3.0);
+    try testing.expectApproxEqAbs(a1.magnitude(), @sqrt(14.0), Tuple(f32).tolerance);
 
-    a1 = Tuple.new_vec3(-1.0, -2.0, -3.0);
-    try testing.expectApproxEqAbs(a1.magnitude(), @sqrt(14.0), tolerance);
+    a1 = Tuple(f32).new_vec3(-1.0, -2.0, -3.0);
+    try testing.expectApproxEqAbs(a1.magnitude(), @sqrt(14.0), Tuple(f32).tolerance);
 
     // normalization
-    a1 = Tuple.new_vec3(4.0, 0.0, 0.0);
-    try testing.expect(a1.normalized().approx_equal(Tuple.new_vec3(1.0, 0.0, 0.0)));
+    a1 = Tuple(f32).new_vec3(4.0, 0.0, 0.0);
+    try testing.expect(a1.normalized().approx_equal(Tuple(f32).new_vec3(1.0, 0.0, 0.0)));
 
-    a1 = Tuple.new_vec3(1.0, 2.0, 3.0);
-    try testing.expect(a1.normalized().approx_equal(Tuple.new_vec3(0.26726, 0.53452, 0.80178)));
+    a1 = Tuple(f32).new_vec3(1.0, 2.0, 3.0);
+    try testing.expect(a1.normalized().approx_equal(Tuple(f32).new_vec3(0.26726, 0.53452, 0.80178)));
 
-    a1 = Tuple.new_vec3(1.0, 2.0, 3.0);
-    try testing.expectApproxEqAbs(a1.normalized().magnitude(), 1.0, tolerance);
+    a1 = Tuple(f32).new_vec3(1.0, 2.0, 3.0);
+    try testing.expectApproxEqAbs(a1.normalized().magnitude(), 1.0, Tuple(f32).tolerance);
 
     // dot product
-    a1 = Tuple.new_vec3(1.0, 2.0, 3.0);
-    a2 = Tuple.new_vec3(2.0, 3.0, 4.0);
-    try testing.expectApproxEqAbs(a1.dot(a2), 20.0, tolerance);
+    a1 = Tuple(f32).new_vec3(1.0, 2.0, 3.0);
+    a2 = Tuple(f32).new_vec3(2.0, 3.0, 4.0);
+    try testing.expectApproxEqAbs(a1.dot(a2), 20.0, Tuple(f32).tolerance);
 
     // cross product
-    a1 = Tuple.new_vec3(1.0, 2.0, 3.0);
-    a2 = Tuple.new_vec3(2.0, 3.0, 4.0);
-    try testing.expect(a1.cross(a2).approx_equal(Tuple.new_vec3(-1.0, 2.0, -1.0)));
+    a1 = Tuple(f32).new_vec3(1.0, 2.0, 3.0);
+    a2 = Tuple(f32).new_vec3(2.0, 3.0, 4.0);
+    try testing.expect(a1.cross(a2).approx_equal(Tuple(f32).new_vec3(-1.0, 2.0, -1.0)));
     try testing.expect(a2.cross(a1).approx_equal(a1.cross(a2).negate()));
 }
